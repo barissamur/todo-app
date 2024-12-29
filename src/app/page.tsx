804f926a9b68bc662de94c4349738f8b257a1dcf
Hasan Barış Samur <barissamurr@gmail.com>
2024-12-29 20:55:40 +0300

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,157 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+
+import { v4 as uuidv4 } from "uuid"; // Benzersiz ID oluşturmak için uuid kütüphanesi
+import { useCallback, useState } from "react";
+import { MdDelete } from "react-icons/md";
+
+// Todo class tanımı
+class Todo {
+  id: string;
+  task: string;
+  completed: boolean;
+
+  constructor(task: string) {
+    this.id = uuidv4();
+    this.task = task;
+    this.completed = false;
+  }
+
+  toggleCompleted() {
+    this.completed = !this.completed;
+  }
+}
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [inputValue, setInputValue] = useState("");
+  const pendingTodos = todos.filter((todo) => !todo.completed); // Bekleyenler
+  const completedTodos = todos.filter((todo) => todo.completed); // Tamamlananlar
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+  const addTodo = () => {
+    if (inputValue.trim() !== "") {
+      const newTodo = new Todo(inputValue); // Yeni Todo oluştur
+      setTodos([...todos, newTodo]); // Listeye ekle
+      setInputValue("");
+    }
+  };
+
+  const handleTodoClick = useCallback(
+    (id: string) => {
+      const updatedTodos = todos.map((todo) => {
+        if (todo.id === id) {
+          const updatedTodo = new Todo(todo.task); // Yeni bir Todo oluştur
+          updatedTodo.completed = !todo.completed; // Durumu tersine çevir
+          return updatedTodo;
+        }
+        return todo; // Diğer görevleri olduğu gibi döndür
+      });
+      setTodos(updatedTodos);
+    },
+    [todos]
+  );
+
+  const handleDeleteTodo = useCallback(
+    (id: string) => {
+      const updatedTodos = todos.filter((todo) => todo.id !== id);
+      setTodos(updatedTodos);
+    },
+    [todos]
+  );
+
+  return (
+    <div className="container mt-5">
+      <h1 className="text-center text-primary mb-4">Todo Uygulaması</h1>
+      <div className="row">
+        {/* sol sütun */}
+        <div className="col-6 border-end border-3 border-secondary">
+          <div className="h1 col-md-8 text-center offset-md-2">
+            Bekleyen Görevler
+          </div>
+          <div className="col-md-8 offset-md-2">
+            <div className="input-group mb-3">
+              <input
+                type="text"
+                className="form-control"
+                value={inputValue}
+                onChange={(e) => setInputValue(e.target.value)}
+                placeholder="Yeni bir todo ekle"
+              />
+              <button className="btn btn-success" onClick={addTodo}>
+                Ekle
+              </button>
+            </div>
+            <ul className="list-group">
+              {pendingTodos.map((todo, index) => (
+                <li
+                  className="list-group-item d-flex justify-content-between align-items-center"
+                  key={todo.id}
+                  onClick={() => handleTodoClick(todo.id)}
+                  style={{ cursor: "pointer" }}
+                >
+                  <span
+                    style={{
+                      textDecoration: todo.completed ? "line-through" : "none",
+                    }}
+                  >
+                    {todo.task}
+                  </span>
+                  <div className="d-flex align-items-center">
+                    <MdDelete
+                      className="ms-3"
+                      size={24}
+                      color="red"
+                      style={{ cursor: "pointer" }}
+                      onClick={(e) => {
+                        e.stopPropagation(); // Parent'ın onClick olayını engeller
+                        handleDeleteTodo(todo.id); // İlgili görevi siler
+                      }}
+                    />
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        {/* sağ sütun */}
+        <div className="col-6">
+          <div className="h1 col-md-8 text-center offset-md-2">
+            Tamamlanan Görevler
+          </div>
+          <ul className="list-group">
+            {completedTodos.map((todo, index) => (
+              <li
+                className="list-group-item d-flex justify-content-between align-items-center"
+                key={todo.id}
+                onClick={() => handleTodoClick(todo.id)}
+                style={{ cursor: "pointer" }}
+              >
+                <span
+                  style={{
+                    textDecoration: todo.completed ? "line-through" : "none",
+                  }}
+                >
+                  {todo.task}
+                </span>
+                <div className="d-flex align-items-center">
+                  <MdDelete
+                    className="ms-3"
+                    size={24}
+                    color="red"
+                    style={{ cursor: "pointer" }}
+                    onClick={(e) => {
+                      e.stopPropagation(); // Parent'ın onClick olayını engeller
+                      handleDeleteTodo(todo.id); // İlgili görevi siler
+                    }}
+                  />
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="col-md-8 offset-md-2 mt-3">
+          Bekleyen Görev Sayısı: {pendingTodos.length}
+        </div>
+      </div>
     </div>
   );
 }
